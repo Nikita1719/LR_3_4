e924a51335dabef70c02ba34069040a64a3f42dc
Nikita1719 <144107423+Nikita1719@users.noreply.github.com>
2023-10-13 11:55:23 +0300

    Add files via upload

---
+++ lr3_4_10.pas
@@ -0,0 +1,12 @@
+﻿program lr3_4_10;
+var t, s1, s2, k:integer;
+begin
+  k:=0;
+  for t:= 0 to 999999 do
+  begin
+    s1 := (t div 100000) mod 10 + (t div 10000) mod 10 + (t div 1000) mod 10;
+    s2 := (t div 100) mod 10 + (t div 10) mod 10 + t mod 10;
+    if (s1=s2) and (s1=13) then k:=k+1;
+  end;
+  writeln( k);
+end.
---
+++ lr3_4_12.pas
@@ -0,0 +1,21 @@
+﻿program lr3_4_12;
+var s, f, n, sum, p: integer;
+
+begin
+  write(' начало диапазона: ');
+  readln(s);
+  write('конец диапазона: ');
+  readln(f);
+  sum := 0;
+  p := 1;
+  for n:= s to f do
+  begin
+    if n mod 2 <> 0 then
+      sum := sum + n
+    else
+      p := p * n;
+  end;
+
+  writeln('Сумма нечетных чисел: ', s);
+  writeln('Произведение четных чисел: ', p);
+end.
